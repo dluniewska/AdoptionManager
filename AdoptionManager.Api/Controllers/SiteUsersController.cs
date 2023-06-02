2a2bd8891db0f3d5cae5c71645091e90044e39d7
Daria Łuniewska <luniewska.d@gmail.com>
2023-06-02 10:10:30 +0200

    created users controller

--- AdoptionManager.Api/Controllers/SiteUsersController.cs
+++ AdoptionManager.Api/Controllers/SiteUsersController.cs
@@ -4,58 +4,58 @@ using System.Net.Mime;
 namespace AdoptionManager.Api.Controllers
 {
     /// <summary>
-    /// Controller for managing organization
+    /// Controller for managing site users
     /// </summary>
     [Route("api/[controller]")]
     [ApiController]
-    public class OrganizationsController : ControllerBase
+    public class SiteUsersController : ControllerBase
     {
         /// <summary>
-        /// Get list of organizations
+        /// Get list of site users
         /// </summary>
         /// <returns></returns>
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<object>))]
-        public async Task<IActionResult> GetOrganizations()
+        public async Task<IActionResult> GetSiteUsers()
         {
             return Ok();
         }
 
         /// <summary>
-        /// Create new organization object
+        /// Create new site user object
         /// </summary>
-        /// <param name="organization"></param>
-        /// <returns>Created organization</returns>
+        /// <param name="siteUser"></param>
+        /// <returns>Created site user</returns>
         [HttpPost]
         [Consumes(MediaTypeNames.Application.Json)]
         [ProducesResponseType(StatusCodes.Status201Created)]
-        public async Task<IActionResult> AddOrganization(object organization)
+        public async Task<IActionResult> AddSiteUser(object siteUser)
         {
-            return Ok(organization);
+            return Ok(siteUser);
         }
 
         /// <summary>
-        /// Update organization by Id
+        /// Update site users by Id
         /// </summary>
-        /// <param name="organizationId"></param>
-        /// <param name="organization"></param>
+        /// <param name="siteUserId"></param>
+        /// <param name="siteUser"></param>
         /// <returns></returns>
-        [HttpPut("{organizationId}")]
+        [HttpPut("{siteUserId}")]
         [Consumes(MediaTypeNames.Application.Json)]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
-        public async Task<IActionResult> UpdateOrganization(int organizationId, object organization)
+        public async Task<IActionResult> UpdateSiteUser(int siteUserId, object siteUser)
         {
             return Ok();
         }
 
         /// <summary>
-        /// Delete organization by Id
+        /// Delete site user by Id
         /// </summary>
-        /// <param name="organizationId"></param>
+        /// <param name="siteUserId"></param>
         /// <returns></returns>
-        [HttpDelete("{organizationId}")]
+        [HttpDelete("{siteUserId}")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
-        public async Task<IActionResult> DeleteOrganization(int organizationId)
+        public async Task<IActionResult> DeleteSiteUser(int siteUserId)
         {
             return Ok();
         }
