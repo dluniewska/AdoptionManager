7c78d318f2d8b1d35698c6607ad03100ab9c1ecc
Daria Łuniewska <luniewska.d@gmail.com>
2023-08-20 22:12:14 +0200

    added address configuration

---
+++ AdoptionManager.Persistance/Configurations/Users/AddressConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using AdoptionManager.Domain.Entities.Users.Addresses;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AdoptionManager.Persistance.Configurations.Users
+{
+    public class AddressConfiguration : IEntityTypeConfiguration<Address>
+    {
+        public void Configure(EntityTypeBuilder<Address> builder)
+        {
+            builder.Property(a => a.Id).UseIdentityColumn();
+            builder.OwnsOne(o => o.AddressObj);
+        }
+    }
+}
